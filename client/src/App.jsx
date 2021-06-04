133acbf792acc3443321a5e3c0b898a6ebdc2aef
Chris McClelland <clm727@gmail.com>
2021-06-03 20:15:23 -0400

    initial setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,1 +1,21 @@
+import React from 'react';
+import axios from 'axios';
 
+const App = () => {
+  const [selectedCow, setSelectedCow] = React.useState({ cowName: 'betty', cowDescription: 'a pretty cool cow' });
+  const [cowList, setCowList] = React.useState([]);
+
+  const eventHandler = () => {
+
+  };
+
+  return (
+    <div>
+      <List cowList={cowList}/>
+      <button onClick={eventHandler}></button>
+    </div>
+  );
+};
+
+
+export default App;
