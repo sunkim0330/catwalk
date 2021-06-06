99ca41cb520f126adb99da85a4f9b75fb5a92c83
Chris McClelland <clm727@gmail.com>
2021-06-06 13:52:32 -0400

    add basic html structure, newest and helpful sort

--- client/src/Reviews/index.jsx
+++ client/src/Reviews/index.jsx
@@ -11,7 +11,7 @@ const Reviews = ({ product }) => {
   const [avgRating, setAvgRating] = useState(0);
   const [meta, setMeta] = useState([]);
   const [filter, setFilter] = useState('');
-  const [sort, setSort] = useState('');
+  const [sort, setSort] = useState('relevant');
 
   const getReviews = () => {
     axios.get(`/reviews/?product_id=${product.id}&count=100`)
@@ -41,11 +41,81 @@ const Reviews = ({ product }) => {
     }
   }, [product.id]);
 
+  const sortReviews = (order) => {
+    const calculateRelevance = (review) => {
+
+    };
+
+    if (order === 'relevant') {
+
+    }
+
+    if (order === 'helpful') {
+      let helpfulSort = reviews.sort((a, b) => {
+        return b.helpfulness - a.helpfulness;
+      });
+      setReviews(helpfulSort);
+    }
+
+    if (order === 'newest') {
+      let newSort = reviews.sort((a, b) => {
+        return new Date(b.date) - new Date(a.date);
+      });
+      setReviews(newSort);
+    }
+  };
+
+  const handleSort = (e) => {
+    setSort(e.target.value);
+    sortReviews(e.target.value);
+  };
+
+  // Will also need this in Q and A section
+  const setDateFormat = () => {
+    reviews.forEach((review) => {
+      review.date = new Date(review.date).toLocaleDateString({}, {month: 'long', day: '2-digit', year: 'numeric'});
+    });
+  };
 
 
   return (
-    <div>
-      Reviews
+    <div id="container">
+
+      {/* container for average rating, reviews breakdown, recommends, characteristics */}
+      <div id="ratings-breakdown">
+        <div>
+          <span>RATINGS & REVIEWS</span>
+          <div>
+            {avgRating}
+          </div>
+          <div>
+            stars go here
+          </div>
+        </div>
+
+        {/* move breakdown and char divs to their components? */}
+        <div id="breakdown">
+          <Breakdown />
+        </div>
+
+        <div id="characteristics">
+          <Characteristics />
+        </div>
+
+      </div>
+
+      {/* container for sort dropdown, reviews, add review button */}
+      <div id="reviews">
+        <div>
+          <span>{reviews.length}</span>
+          <select id="sort" value={sort} onChange={handleSort}>
+            <option value="relevant">Relevant</option>
+            <option value="helpful">Helpful</option>
+            <option value="newest">Newest</option>
+          </select>
+        </div>
+      </div>
+
     </div>
   );
 };
