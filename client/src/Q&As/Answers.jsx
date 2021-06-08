25a56b084b99d68df30c8840c4be06cb91830be8
Sun Kim <sunkim0205@gmail.com>
2021-06-08 12:53:28 -0500

    start working on button function 'load more ...' for quetions AND answers

--- client/src/Q&As/Answers.jsx
+++ client/src/Q&As/Answers.jsx
@@ -1,8 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import axios from 'axios';
 
-const Answers = ({ questions, product }) => {
+const Answers = ({ questions }) => {
   const [answers, setAnswer] = useState([]);
+  const [loadPage, setLoadPage] = useState(2);
 
   useEffect(() => {
     if (questions) {
@@ -16,15 +17,26 @@ const Answers = ({ questions, product }) => {
     }
   }, [questions]);
 
-  //load up to 2 answers on the page
-  //need to figure out how to hide rest of answers and show them when I click 'load more answers'
+  const loadMore = useCallback(() => {
+    setLoadPage(prev => prev + 2);
+  }, []);
+
+
+  //couldn't figure out how to reuse loadMore function from Question component.
+  //I tried e.stopProgation many different ways, but didn't figure out yet.
+  //I'll try to refactor when I finish with eveything
+  const loadAnswers = answers.slice(0, loadPage).map((answer, index) => {
+    return (
+      <div className="answer_div" key={answer.answer_id}>
+            A: {answer.body}
+      </div>
+    );
+  });
+
   return (
     <div>
-      {answers.map((answer, index) => {
-        return <div className="answer_div" key={answer.answer_id}>
-          A: {answer.body}
-        </div>;
-      })}
+      {loadAnswers}
+      <button className="answer_button" onClick={loadMore}>See more answers</button>
     </div>
   );
 };
@@ -32,9 +44,26 @@ const Answers = ({ questions, product }) => {
 export default Answers;
 
 /*
+When expanded, the button to “See more answers” should change to read “Collapse answers”.
+*/
+
+/*
 If time allows, answers should have the capability of supporting image uploads.  If an answer submitted includes images, thumbnail images for each image submitted should appear below the answer text body, above the username and other metadata.
 Each image thumbnail should be clickable.  Upon clicking the thumbnail, a modal window expanding the image at full resolution should appear over the page.  The only functionality within this modal window should be an “X” icon through which the user can close out of the modal.
 
  <img src={answer.photos.url} />
   something lik
+*/
+
+/*
+return (
+      <div>
+        {answers.map((answer, index) => {
+          return <div className="answer_div" key={answer.answer_id}>
+            A: {answer.body}
+          </div>;
+        })}
+        {loadPage && <button onClick={loadMore}>See more answers</button>}
+      </div>
+    );
 */
--- client/src/Q&As/Questions.jsx
+++ client/src/Q&As/Questions.jsx
@@ -1,19 +1,62 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
 import axios from 'axios';
 import Answers from './Answers.jsx';
 
-const Questions = ({ questions }) => {
-  //load up to 4 questions. didn't make a function for that yet
+const Questions = ({ product }) => {
+  const [questions, setQuestions] = useState([]);
+  const [loadPage, setLoadPage] = useState(2);
+
+  useEffect(() => {
+    if (product.id) {
+      axios.get(`/qa/questions?product_id=${product.id}`)
+        .then((response) => {
+          return setQuestions(response.data.results);
+        })
+        .catch(() => {
+          console.log('cant get request from question API');
+        });
+    }
+  }, [product.id]);
+
+  const loadMore = useCallback(() => {
+    setLoadPage(prev => prev + 2);
+  }, []);
+
+  const loadQuestions = questions.slice(0, loadPage).map((question, index) => {
+    return (
+      <div className="questions_div" key={index}>
+        Q: {question.question_body}<br/>
+        <Answers questions={question.question_id} />
+      </div>
+    );
+  });
+
   return (
     <div>
-      {questions.map((question, index) => {
-        return <div className="questions_div" key={index}>
-          Q: {question.question_body}<br/>
-          <Answers questions={question.question_id}/>
-        </div>;
-      })}
+      {loadQuestions}
+      <button
+        style = {{display: loadPage >= questions.length ? 'none' : 'block'}}
+        onClick={loadMore}>MORE ANSWERED QUESTIONS</button>
     </div>
   );
+
+
 };
 
 export default Questions;
+
+/*
+The list will by default only display up to 2 questions asked.
+If there are 2 or less questions for the given product, then the button will not appear.
+-- keeping this just in case I want to refactor loading button again
+ <div>
+  {questions.map((question, index) => {
+    return <div className="questions_div" key={index}>
+      Q: {question.question_body}<br/>
+      <Answers questions={question.question_id}/>
+    </div>;
+  })}
+</div>
+
+onClick={e => e.stopPropagation()}
+*/
