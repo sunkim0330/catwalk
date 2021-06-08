71a700dad3c3f484826ead61dc326ebdda4e30d8
Alex Armstrong <alexaarms96@gmail.com>
2021-06-08 14:52:08 -0400

    Added Function to get product ratings and styles - added function to calculate and set average rating and set total reviews state

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,6 +12,32 @@ const App = () => {
   const [reviewMetaData, setReviewMetaData] = useState({});
   const [averageRating, setAverageRating] = useState(5);
   const [currentStyles, setCurrentStyles] = useState();
+  const [totalReviewCount, setTotalReviewCount] = useState(0);
+
+  const getProductRatingsAndStyles = () => {
+    axios.get(`/reviews/meta?product_id=${currentProduct.id}`)
+      .then((response) => {
+        console.log(response.data);
+        let ratings = response.data.ratings;
+        calculateReviewAverage(ratings);
+        setReviewMetaData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const calculateReviewAverage = (ratings) => {
+    let first = 0;
+    let second = 0;
+    for (let reviews in ratings) {
+      first += (Number(reviews) * Number(ratings[reviews]));
+      second += Number(ratings[reviews]);
+    }
+    setTotalReviewCount(second);
+    let average = first / second;
+    setAverageRating(average.toFixed(1));
+  };
 
   useEffect(() => {
     axios.get('/products/19089')
@@ -19,6 +45,12 @@ const App = () => {
         setCurrentProduct(response.data);
       });
   }, []);
+
+  useEffect(() => {
+    if (currentProduct.id) {
+      getProductRatings();
+    }
+  }, [currentProduct.id]);
 
   return (
     <div>
