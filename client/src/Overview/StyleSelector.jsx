a2a9aca8350ea95fd4caee34d2c9f0e2996846c8
Alex Armstrong <alexaarms96@gmail.com>
2021-06-07 23:34:19 -0400

    Added style name to info display - changed handler name

--- client/src/Overview/StyleSelector.jsx
+++ client/src/Overview/StyleSelector.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
-const StyleSelector = ({ styles, styleSelector }) => {
+const StyleSelector = ({ styles, setCurrentStyle, currentStyle }) => {
 
   return (
     <div id="styleSelector">
       <h3>Style Selector</h3>
+      <div>{'Style Name > '}{currentStyle.name}</div>
       {styles.map((style, index) => {
-        return <img src={style.photos[0].thumbnail_url} alt={style.name + 'Style'} key={index} onClick={() => styleSelector(index)}/>;
+        return <img src={style.photos[0].thumbnail_url} alt={style.name + 'Style'} key={index} onClick={() => setCurrentStyle(index)}/>;
       })}
     </div>
   );
