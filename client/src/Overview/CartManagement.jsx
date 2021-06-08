4d094df8c2d5b8d793e40ce4c8864b44544114ba
Alex Armstrong <alexaarms96@gmail.com>
2021-06-08 14:06:37 -0400

    Cart Management Modifications - in progress

--- client/src/Overview/CartManagement.jsx
+++ client/src/Overview/CartManagement.jsx
@@ -4,49 +4,75 @@ import axios from 'axios';
 const CartManagement = ({ styleInventory }) => {
 
   const [productSku, setProductSku] = useState();
-  const [productSize, setProductSize] = useState();
   const [availableQty, setAvailableQty] = useState(0);
   const [purchaseQty, setPurchaseQty] = useState(0);
+  const [sizeOptions, setSizeOptions] = useState([]);
+  const [quantityOptions, setQuantityOptions] = useState([]);
+  const [formattedSkuData, setFormattedSkuData] = useState([]);
 
-  let formattedSkuData = [];
-
-  for (let skus in styleInventory) {
-    let currentSkuData = {};
-    currentSkuData.sku = skus;
-    currentSkuData.quantity = styleInventory[skus].quantity;
-    currentSkuData.size = styleInventory[skus].size;
-    formattedSkuData.push(currentSkuData);
-  }
-
-  let sizeOptions = formattedSkuData.map((sku, index) => {
-    if (sku.quantity) {
-      return <option value={index} key={index}>{sku.size}</option>;
+  useEffect(() => {
+    if (styleInventory) {
+      formatSkuData();
     }
-  });
+  }, [styleInventory]);
 
-  sizeOptions.length === 0 ? sizeOptions = <option>OUT OF STOCK</option> : null;
+  useEffect(() => {
+    productSku ? setPurchaseQty(1) : setPurchaseQty(0);
+    generateSizeOptions();
+    generateQuantityOptions();
+  }, [formattedSkuData, productSku]);
 
-  let quantityOptions = [];
+  const formatSkuData = (newSkuData = []) => {
+    axios.get('/cart')
+      .then((response) => {
+        let cart = response.data;
+        for (let i = 0; i < cart.length; i++) {
+          if (styleInventory[cart[i].sku_id]) {
+            styleInventory[cart[i].sku_id].tempQuantity = styleInventory[cart[i].sku_id].quantity - Number(cart[i].count);
+          }
+        }
+      })
+      .then(() => {
+        for (let sku in styleInventory) {
+          if (styleInventory[sku].tempQuantity === 0) {
+            continue;
+          }
+          styleInventory[sku].sku = sku;
+          newSkuData.push(styleInventory[sku]);
+        }
+        setFormattedSkuData(newSkuData);
+      });
+  };
 
-  while (quantityOptions.length < availableQty && quantityOptions.length < 15) {
-    let numToUse = quantityOptions.length + 1;
-    quantityOptions.push(<option value={numToUse} key={numToUse}>{numToUse}</option>);
-  }
+  const generateSizeOptions = () => {
+    let newSizeOptions = formattedSkuData.map((sku, index) => {
+      if (sku.quantity) {
+        return <option value={index} key={index}>{sku.size}</option>;
+      }
+    });
+    newSizeOptions.length === 0 ? newSizeOptions = <option>OUT OF STOCK</option> : null;
+    setSizeOptions(newSizeOptions);
+  };
+
+  const generateQuantityOptions = (newQuantityOptions = []) => {
+    while (newQuantityOptions.length < availableQty && newQuantityOptions.length < 15) {
+      let optionNum = newQuantityOptions.length + 1;
+      newQuantityOptions.push(<option value={optionNum} key={optionNum}>{optionNum}</option>);
+    }
+    newQuantityOptions.unshift(<option key='yes'>-</option>);
+    setQuantityOptions(newQuantityOptions);
+  };
 
   const handleSizeSelection = (event) => {
-    let index = event.target.value;
-    if (index !== 'Select Size') {
-      setProductSku(formattedSkuData[index].sku);
-      setProductSize(formattedSkuData[index].size);
-      setAvailableQty(formattedSkuData[index].quantity);
-    }
-    if (purchaseQty > availableQty) {
-      setPurchaseQty(availableQty);
+    let skuIndex = event.target.value;
+    if (skuIndex !== 'Select Size') {
+      setProductSku(formattedSkuData[skuIndex].sku);
+      setAvailableQty(formattedSkuData[skuIndex].quantity);
     }
   };
 
-  const handleQuantitySelection = () => {
-
+  const handleQuantitySelection = (event) => {
+    setPurchaseQty(Number(event.target.value));
   };
 
   const addToCart = (quantity) => {
@@ -69,8 +95,7 @@ const CartManagement = ({ styleInventory }) => {
         <option>Select Size</option>
         {sizeOptions}
       </select>
-      <select onChange={(event) => setPurchaseQty(Number(event.target.value))}>
-        <option>-</option>
+      <select id="countselector" onChange={(event) => handleQuantitySelection(event)}>
         {quantityOptions}
       </select>
       <button type="button" onClick={() => addToCart(Number(purchaseQty))}>Add to Cart</button>
