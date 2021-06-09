af04cb64b958806cba4a29b12f276da29248b850
Chris McClelland <clm727@gmail.com>
2021-06-08 20:02:11 -0400

    add summary input, body input, min characters left counter

--- client/src/Reviews/AddReview.jsx
+++ client/src/Reviews/AddReview.jsx
@@ -9,6 +9,18 @@ const AddReview = ({ product, chars, ratings }) => {
     Length: ['Runs short', 'Runs slightly short', 'Perfect', 'Runs slightly long', 'Runs long'],
     Fit: ['Runs tight', 'Runs slightly tight', 'Perfect', 'Runs slightly loose', 'Runs looose']
   });
+  const [summary, setSummary] = useState('');
+  const [body, setBody] = useState('');
+  const [minRequiredChars, setminRequiredChars] = useState(50);
+
+  const handleBodyChange = (e) => {
+    //let newBody = e.target.value.length;
+    setBody(e.target.value);
+    setminRequiredChars(() => {
+      var chars = e.target.value.length;
+      return chars < 50 ? 50 - chars : 0;
+    });
+  };
 
   const renderCharButtons = (char) => {
     return (
@@ -84,6 +96,43 @@ const AddReview = ({ product, chars, ratings }) => {
               {chars.map((char) => {
                 return (renderCharButtons(char));
               })}
+            </div>
+          </div>
+
+          <div>
+            <label>
+              Review summary
+            </label>
+            <input
+              type="text"
+              id="add-review-summary"
+              name="add-review-summary"
+              maxLength="60"
+              placeholder="Example: Best purchase ever!"
+              size="30"
+              value={summary}
+              onChange={e => setSummary(e.target.value)}
+              required
+            />
+          </div>
+
+          <div>
+            <label>
+              Review
+            </label>
+            <textarea
+              id="add-review-body"
+              name="add-review-body"
+              rows="5"
+              columns="30"
+              minLength="50"
+              maxLength="1000"
+              placeholder="Why did you like the product or not?"
+              value={body}
+              onChange={handleBodyChange}
+            />
+            <div>
+             Minimum required characters left: {minRequiredChars}
             </div>
           </div>
 
