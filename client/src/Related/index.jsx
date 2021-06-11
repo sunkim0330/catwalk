13f5a9d1ed8e3fb7b6f158c73e226e11f4449b8b
Katherine Darby <katherinemdarby@gmail.com>
2021-06-11 13:42:17 -0400

    First try at interactions reporting

--- client/src/Related/index.jsx
+++ client/src/Related/index.jsx
@@ -5,7 +5,17 @@ import OutfitList from './OutfitList.jsx';
 
 
 const Related = (props) => {
+
+  const getClickedElement = (event) => {
+    const module = 'Related Items and Comparison';
+    axios.post('/interactions', {element: event.target.localName, widget: module, time: new Date() })
+      .then((response) => {
+        console.log(response);
+      });
+  };
+
   return (
+    // <div onClick={getClickedElement}>
     <div>
       <RelatedList product={props.product} setCurrentProduct={props.setProduct} currentStyle={props.defaultStyle} currentChar={props.currentChar}/>
       <OutfitList product={props.product} style={props.defaultStyle} rating={props.rating} />
