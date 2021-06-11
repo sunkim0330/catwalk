394bb2d7614fd0c24909bf67f095bb983a131ca2
Alex Armstrong <alexaarms96@gmail.com>
2021-06-11 15:15:36 -0400

    Added Interaction Tracker

--- client/src/Overview/index.jsx
+++ client/src/Overview/index.jsx
@@ -11,8 +11,16 @@ const Overview = ({ product, styles, defaultStyle, totalReviews, averageRating }
 
   const [currentStyle, setCurrentStyle] = useState(defaultStyle);
 
+  const getClickedElement = (event) => {
+    const module = 'Overview';
+    axios.post('/interactions', {element: event.target.localName, widget: module, time: new Date() })
+      .then((response) => {
+        console.log('We\'re stalking you!! :D');
+      });
+  };
+
   return (
-    <Styles.Overview>
+    <Styles.Overview onClick={getClickedElement}>
       <Gallery styleImages={styles[currentStyle].photos} productID={product.id}/>
       <ProductInfo product={product} style={styles[currentStyle]} rating={averageRating} reviewCount={totalReviews}/>
       <StyleSelector styles={styles} setCurrentStyle={setCurrentStyle} currentStyle={styles[currentStyle]}/>
