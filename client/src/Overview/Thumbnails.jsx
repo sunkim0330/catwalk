5b4683b3b78bd01aa3e7cb51fea275d9f9ba9785
Alex Armstrong <alexaarms96@gmail.com>
2021-06-10 23:38:32 -0400

    Implemented a lot of gallery/thumbnail functionality - some styling - changed prop name in overview index

--- client/src/Overview/Thumbnails.jsx
+++ client/src/Overview/Thumbnails.jsx
@@ -1,21 +1,11 @@
 import React, { useState, useEffect} from 'react';
 import * as Styles from './styledComponents.js';
 
-const Thumbnails = ({ styleThumbnails, imageIndex, changeMainImage }) => {
-
-  const [renderedThumbnails, setRenderedThumbnails] = useState(styleThumbnails.slice(0, 7));
-
-  const updateRenderedThumbnails = () => {
-
-  };
-
-  useEffect(() => {
-
-  }, [imageIndex]);
+const Thumbnails = ({ styleImages, imageIndex, changeMainImage }) => {
 
   return (
     <Styles.Thumbnails>
-      {styleThumbnails.map((stylePhoto, index) => {
+      {styleImages.map((stylePhoto, index) => {
         return <Styles.GalleryThumbnail src={stylePhoto.thumbnail_url} key={stylePhoto.thumbnail_url} onClick={() => changeMainImage(index)}/>;
       })}
     </Styles.Thumbnails>
