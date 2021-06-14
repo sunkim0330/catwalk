6debe57ee8ab56ae1485798b878b09690139ada0
Alex Armstrong <alexaarms96@gmail.com>
2021-06-14 16:44:34 -0400

    Pinterest and Facebook buttons functional

--- client/src/Overview/ShareButtons.jsx
+++ client/src/Overview/ShareButtons.jsx
@@ -5,8 +5,14 @@ const ShareButtons = () => {
   return (
     <Styles.Socials id="socials">
       <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" className="twitter-share-button" data-show-count="false" target="_blank"><Styles.Twitter>Tweet Product</Styles.Twitter></a><script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>
-      <Styles.Pinterest>Pin Product</Styles.Pinterest>
-      <Styles.Facebook>The Facebook</Styles.Facebook>
+
+      <a class="w-inline-block social-share-btn pin" href="http://pinterest.com/pin/create/button/?url=&description=" target="_blank" title="Pin it" onclick="window.open('http://pinterest.com/pin/create/button/?url=' + encodeURIComponent(document.URL) + '&description=' + encodeURIComponent(document.title)); return false;">
+        <Styles.Pinterest>Pin Product</Styles.Pinterest>
+      </a>
+
+      <a class="w-inline-block social-share-btn fb" href="https://www.facebook.com/sharer/sharer.php?u=&t=" title="Share on Facebook" target="_blank" onclick="window.open('https://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent(document.URL) + '&t=' + encodeURIComponent(document.URL)); return false;">
+        <Styles.Facebook>The Facebook</Styles.Facebook>
+      </a>
     </Styles.Socials>
   );
 };
