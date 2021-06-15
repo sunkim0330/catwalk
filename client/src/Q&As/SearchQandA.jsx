7e71f0edbb3f62702ba4a6035331e28a9db4a7df
Sun Kim <sunkim0205@gmail.com>
2021-06-14 21:36:32 -0500

    rework on search bar and questions components. I combined them into one component. Still working on css-grid

---
+++ client/src/Q&As/SearchQandA.jsx
@@ -0,0 +1,78 @@
+import React, {useState, useEffect, useCallback} from 'react';
+import axios from 'axios';
+import Helpful from '../Shared/Helpful.jsx';
+import Modal from './Modal.jsx';
+import * as Styles from './Styles.js';
+import Answers from './Answers.jsx';
+
+const SearchQandA = ({product, setDateFormat}) => {
+  const [questions, setQuestions] = useState([]);
+  const [loadPage, setLoadPage] = useState(2);
+  const [search, setSearch] = useState('');
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    axios.get(`/qa/questions?product_id=${product.id}&count=100`)
+      .then((response) => {
+        return setQuestions(response.data.results);
+      })
+      .catch(() => {
+        console.log('cant get request from question API');
+      });
+  }, [product.id]);
+
+  const sortQuestions = () => {
+    const sorted = questions.sort((a, b) => {
+      return b.question_helpfulness > a.question_helpfulness;
+    });
+    setQuestions(sorted);
+  };
+
+  const filteredQuestion = questions.filter((val) => {
+    if (search === '' || search.length < 3 ) {
+      return val;
+    } else if (val.question_body.toLowerCase().includes(search.toLowerCase())) {
+      return val;
+    }
+  }).slice(0, loadPage).map((question, key) => {
+    return (
+      <div className="questions_div" key={question.question_id}>
+        <Styles.QuestionsContainer>
+          Q: {question.question_body} <Styles.addAnswerButton onClick={() => setShow(true)} >Add Answer</Styles.addAnswerButton><Helpful origin="qa/questions" id={question.question_id} helpCount={question.question_helpfulness}/>
+          <Modal title="Submit Your Answer" subTitle={product.name} id={question.question_id} questionBody={question.question_body} show={show} onClose={() => setShow(false)}/><br/>
+          <div><Answers product={product} questions={question} setDateFormat={setDateFormat} /></div>
+        </Styles.QuestionsContainer>
+      </div>
+    );
+  });
+
+
+
+  useEffect(() => {
+    sortQuestions();
+  }, [questions]);
+
+  const loadMore = useCallback(() => {
+    setLoadPage(prev => prev + 2);
+  }, []);
+
+  return (
+    <div>
+      <Styles.SearchBarInput onChange={(e) => setSearch(e.target.value)}
+        type="text"
+        placeholder="Have a question? Search for answers..."
+      />
+      {filteredQuestion}
+      <Styles.moreQuestionButton
+        style = {{display: loadPage >= questions.length ? 'none' : 'block'}}
+        className="question_button" onClick={loadMore}>
+        MORE ANSWERED QUESTIONS
+      </Styles.moreQuestionButton>
+    </div>
+  );
+};
+
+
+export default SearchQandA;
+
+
--- client/src/Q&As/Styles.js
+++ client/src/Q&As/Styles.js
@@ -2,30 +2,62 @@ import React from 'react';
 import styled, {css} from 'styled-components';
 
 
-export const Index = styled.div`
-  width: 80%
+export const IndexGrid = styled.div`
+  padding: 5px;
   font-family: Sans-Serif;
   font-size: 18px;
-  border: 1px solid white;
-  font-color: #6f67bf;
+  border: 1px solid grey;
+  display: grid;
+  color: #6f67bf;
+`;
+
+export const moreQuestionButton = styled.button`
+  border: transparent;
+  align-self: end;
+  grid-area: morequestions;
+  background-color: transparent;
+  font-family: sans-serif;
+  font-size: 15px;
+  padding: 5px;
+  cursor: pointer;
+  &:hover {
+    color: #b3225e;
+  }
+`;
+
+export const askQuestionButton = styled.button`
+  border: transparent;
+  align-self: end;
+  grid-area: askquestion;
+  background-color: transparent;
+  font-family: sans-serif;
+  font-size: 15px;
+  padding: 5px;
+  cursor: pointer;
+  &:hover {
+    color: #b3225e;
+  }
 `;
 
 export const QandAsInput = styled.div.attrs({className: 'QandAsInput'})`
-  width: 80%
+  width: 80%;
   font-family: Sans-Serif;
-  font-color: #6f67bf;
+  color: #6f67bf;
   font-size: 15px;
 `;
 
 export const QuestionsContainer = styled.div.attrs({className: 'Questions_container'})`
-  width: 80%
+  width: 80%;
   font-family: Sans-Serif;
-  font-color: #6f67bf;
+  color: #6f67bf;
   font-size: 15px;
-  // font-weight: bold
+  font-weight: bold;
 `;
 
 export const SearchBarInput = styled.input`
+  display: grid;
+  grid-column-start: 1;
+  grid-column-end: five
   font-size: 14px;
   padding: 2px 5px;
   border: 1px solid grey;
@@ -37,12 +69,64 @@ export const SearchBarInput = styled.input`
   font-family: Sans-Serif;
 `;
 
+export const answerContainer = styled.div`
+grid-area: answer;
+width: 80%;
+font-family: Sans-Serif;
+color: #6f67bf;
+font-size: 15px;
+font-weight: normal;
+`;
+
+export const answerFooter = styled.div`
+  grid-area: answerhelpful;
+  font-family: sans-serif;
+  font-size: 10px;
+  color: grey;
+  font-weight: normal;
+`;
+
 export const addAnswerButton = styled.button`
+  grid-area: helpful;
   border: transparent;
   background-color: transparent;
   font-family: sans-serif;
+  cursor: pointer;
+  &:hover {
+    color: #b3225e;
+  }
+`;
+export const moreAnswerButton = styled.button`
+  grid-area: answer;
+  border: transparent;
+  background-color: transparent;
+  font-family: sans-serif;
+  color: #050094;
+  cursor: pointer;
+  &:hover {
+    color: #b3225e;
+  }
 `;
 
+export const lineBreak = styled.div`
+top: 0px;
+margin:0px;
+padding: 3px 2px 2px 3px;
+border-width: 2px;
+border-bottom-style:solid;
+border-bottom: 2px #F7CAC9 solid;
+width: 80%;
+`;
+
+export const btwnAnswers = styled.div`
+top: 0px;
+margin:0px;
+padding: 3px 2px 2px 3px;
+border-width: 2px;
+border-bottom-style:solid;
+border-bottom: 2px transparent;
+width: 80%;
+`;
 
 export const ModalContainer = styled.div`
   position: fixed;
@@ -78,5 +162,23 @@ export const ModalBody = styled.div.attrs({className: 'qanda-modal-body'})`
 export const searchResult = styled.div`
   font-family: Sans-Serif;
   font-size: 16px;
-  font-color: #6f67bf;
+  color: #6f67bf;
 `;
+
+/*
+padding: 5px;
+  font-family: Sans-Serif;
+  font-size: 18px;
+  border: 1px solid grey;
+  grid-template-columns: 40px 50px auto 50px 40px;
+  grid-template-rows: 20% 30% 20% 10% auto; //searchbar questions answers buttons
+  column-gap: 15px;
+  row-gap: 15px;
+  grid-template-areas:
+    ". search search search  ."
+    "question question . . helpful"
+    "answer answer answer . ."
+    "answerhelpful answerhelpful answerhelpful answerhelpful . "
+    "morequestions morequestions . askquestion askquestion";
+  color: #6f67bf;
+*/
